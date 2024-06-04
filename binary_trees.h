c32d67a1f4db7715ad3797647abb24a5548239d8
larini abdellah <laribox2014@gmail.com>
2024-06-04 15:33:48 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stddef.h>
 
 
 /**
@@ -33,6 +34,8 @@ typedef struct binary_tree_s avl_t;
 
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 /* prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
