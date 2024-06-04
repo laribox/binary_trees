492964036bf270b3c556ed2cb8b5bbf6113d2745
larini abdellah <laribox2014@gmail.com>
2024-06-04 16:25:07 +0100

    Update 0-binary_tree_node.c added condition in case of alloc failure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,6 +9,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
+	if (!new_node)
+		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->right = NULL;
