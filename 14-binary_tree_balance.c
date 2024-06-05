7aede22a8cb920935ff08c0470749ac95cda3203
laribox <laribox2014@gmail.com>
2024-06-05 16:40:35 -0700

    task 14 node balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,3 @@
-#include "binary_trees.h"
 #include "9-binary_tree_height.c"
 /**
   * binary_tree_balance - Checks to see if the binary tree is balanced
@@ -9,8 +8,11 @@
   */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	int left, right;
+
+	if (!tree)
 		return (0);
-	return (binary_tree_height(tree->left)
-		- binary_tree_height(tree->right));
+	left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+	return (left - right);
 }
