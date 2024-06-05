74eb9d98d05cf0c25000fd4418816cb528cbf85d
laribox <laribox2014@gmail.com>
2024-06-05 16:38:08 -0700

    task 14 node balance

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,22 +1,25 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
-#include "15-binary_tree_is_full.c"
-
+#include "14-binary_tree_balance.c"
 /**
- * binary_tree_is_perfect - checks if tree is perfect
- * @tree: tree to check
- * Return: 1 if it is perfect, 0 otherwise
- */
+  * binary_tree_is_perfect - checks if tree is perfect
+  * @tree: tree to check
+  * Return: 1 if it is perfect
+  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	return (0);
+	int left, right;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	if (left_height != right_height)
+	if (!tree)
 		return (0);
-
-	return (binary_tree_is_full(tree));
+	if (!tree->left && !tree->right)
+		return (1);
+	if (binary_tree_balance(tree) != 0)
+		return (0);
+	if (tree->left && tree->right)
+	{
+		left = binary_tree_is_perfect(tree->left);
+		right = binary_tree_is_perfect(tree->right);
+		return (left && right);
+	}
+	return (0);
 }
