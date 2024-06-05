745d8358dc52dab33e7855b4e286f549425c9a6c
laribox <laribox2014@gmail.com>
2024-06-05 06:34:08 -0700

    task 11 tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_size - returns the size of a binary tree
+  * @tree: tree to measure
+  * Return: size of the tree
+  */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int size = 1;
+
+	if (!tree)
+		return (0);
+
+	size = binary_tree_size(tree->left + tree->right);
+
+	return (size);
+}
