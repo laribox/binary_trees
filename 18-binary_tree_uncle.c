d3e49ecf050d8929cbe323358af2858a2ae1c076
laribox <laribox2014@gmail.com>
2024-06-05 14:13:35 -0700

    task 18 node uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
+
+/**
+  * binary_tree_uncle - finds the parent's sibling of the current node
+  * @node: node to check
+  * Return: pointer to the uncle.
+  */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
